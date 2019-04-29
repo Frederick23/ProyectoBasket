53bcc86da71c7a1e18c65757a7e22f796c1a1af4
Diego Cuenca Arriols <diego.cuenca@metrica.es>
2019-04-29 16:29:19 +0200

    Models 0.5

--- Back/BasketProject/BasketMVC/models.py
+++ Back/BasketProject/BasketMVC/models.py
@@ -4,9 +4,9 @@ from django.db import models
 
 class jugador(models.Model):
     id = models.AutoField(primary_key=True)
-    nombre = models.CharField('Nombre',max_length=30, blank=False)
-    apellido1 = models.CharField('Primer Apellido', max_length=30,blank=False)
-    apellido2 = models.CharField('Segundo Apellido', max_length=30,blank=False)
+    nombre = models.CharField('Nombre', max_length=30, blank=False)
+    apellido1 = models.CharField('Primer Apellido', max_length=30, blank=False)
+    apellido2 = models.CharField('Segundo Apellido', max_length=30, blank=False)
     fecha_nac = models.DateField(blank=False)
 
 class equipo(models.Model):
@@ -23,25 +23,24 @@ class partido(models.Model):
     ASCENSO = 'Fase Ascenso'
 
     FASES = (
-        (PFASE,'Primera Fase'),
-        (SFASE,'Segunda Fase'),
-        (FINAL,'Fase Final'),
-        (ASCENSO,'Fase de Ascenso')
+        (PFASE, 'Primera Fase'),
+        (SFASE, 'Segunda Fase'),
+        (FINAL, 'Fase Final'),
+        (ASCENSO, 'Fase de Ascenso')
     )
 
     id = models.AutoField(primary_key=True)
-    equipo1 = models.ForeignKey(equipo)
-    equipo2 = models.ForeignKey(equipo)
+    equipo1 = models.ForeignKey(equipo, on_delete=models.PROTECT)
+    equipo2 = models.ForeignKey(equipo, on_delete=models.PROTECT)
     fecha = models.DateField(blank=False)
     localizacion = models.CharField(max_length=30)
-    fase =models.CharField(choices=FASES, blank=False)
+    fase = models.CharField(choices=FASES, blank=False)
 
-    cuarto1 = models.CharField(blank=False,max_length=5)
-    cuarto2 = models.CharField(blank=False,max_length=5)
-    cuarto3 = models.CharField(blank=False,max_length=5)
-    cuarto4 = models.CharField(blank=False,max_length=5)
-    tiempos_extra = models.CharField(blank=False,max_length=5)
-    tanteo_final = models.CharField(blank=False,max_length=7)
+    cuarto1 = models.CharField(blank=False, max_length=5)
+    cuarto2 = models.CharField(blank=False, max_length=5)
+    cuarto3 = models.CharField(blank=False, max_length=5)
+    cuarto4 = models.CharField(blank=False, max_length=5)
+    tiempos_extra = models.CharField(blank=False, max_length=5)
+    tanteo_final = models.CharField(blank=False, max_length=7)
 
     jugadores = models.ManyToManyField(jugador, related_name="Jugadores")
-
