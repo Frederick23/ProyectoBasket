1626779366caed12d0a8df1e17c13022bb5d4e56
Diego Cuenca Arriols <diego.cuenca@metrica.es>
2019-04-30 16:30:44 +0200

    FUNCIONA CARAJO

---
+++ Back/BasketProject/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\diego.cuenca\proyectos\Django\ProyectoBasket\Back\BasketProject\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
