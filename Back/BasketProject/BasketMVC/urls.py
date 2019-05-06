0317d390f81db3d120436de5a4c57b27251ca399
Diego Cuenca Arriols <diego.cuenca@metrica.es>
2019-05-06 09:29:37 +0200

    Mostrar_jugadores
    Creada ruta para mostrar jugadores con su propio template

--- Back/BasketProject/BasketMVC/urls.py
+++ Back/BasketProject/BasketMVC/urls.py
@@ -4,5 +4,6 @@ from BasketMVC import views
 
 urlpatterns = [
     path('', views.index, name = 'index'),
+    path('mostrar_jugadores', views.Mostrar, name = 'Mostrar'),
 ]
 
--- Back/BasketProject/BasketMVC/views.py
+++ Back/BasketProject/BasketMVC/views.py
@@ -1,4 +1,11 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from BasketMVC.models import jugador
+
 
 def index(request):
     return HttpResponse("Hola y bienvenido al index")
+
+
+def Mostrar(request):
+    return render(request, 'tabla.html', {'jugadores': jugador.objects.all()})
