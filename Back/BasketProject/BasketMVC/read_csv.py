f71e9471dde298749e8ace4ef2274e0e588e43ec
Frederick23 <35486596+Frederick23@users.noreply.github.com>
2019-05-07 13:21:24 +0200

    Add files via upload

--- Back/BasketProject/BasketMVC/read_csv.py
+++ Back/BasketProject/BasketMVC/read_csv.py
@@ -1,20 +1,57 @@
 import csv
-from BasketMVC.models import partido
-# initializing the titles and rows list
+import re
+
+info_partido = []
+info_puntos = []
+equipo1 = []
+equipo2 = []
 rows = []
+cont = 0
+cont2 = 0
+rangoA1 = 0
+rangoA2 = 0
+rangoB1 = 0
+rangoB2 = 0
 
+with open('prueba.csv', 'r') as f:
+  reader = csv.reader(f)
+  rows = list(reader)
 
-with open('prueba.csv', 'r') as csvfile:
-    csvreader = csv.reader(csvfile)
-    for row in csvreader:
-        rows.append(row)
+for row in rows:
+    fila = re.sub(r'\W+', '', str(row))
+    #print(fila)
+    if fila == "Corazonistas":
+        rangoB1 = cont2
+    elif fila == "CorazonistasResumendeTiros":
+        rangoB2 = cont2
+    elif fila == "DistrtoOlimpico":
+        rangoA1 = cont2
+    elif fila == "DistrtoOlimpicoResumendeTiros":
+        rangoA2 = cont2
+    cont2 += 1
 
+for row in rows:
+    if cont in range(0,2):
+        info_partido.append(row)
+    elif cont in range(4,8):
+        info_puntos.append(row)
+    elif cont in range(rangoA1, rangoA2):
+        equipo1.append(row)
+    elif cont in range(rangoB1, rangoB2):
+        equipo2.append(row)
+    cont += 1
 
-p = partido()
+for fila in equipo1:
+    for celda in fila:
+        if(celda == ""):
+            celda = 0
 
-p.fecha = rows[1][5]
-p.localizacion = rows[2][5]
+##print(equipo1)
 
+print("******************")
 
-print(rows[1][5])
-print(p)
+#for fila in equipo1:
+    #print(fila)
+    #for celda in fila:
+        #print(celda)
+    #print("-----------------")
