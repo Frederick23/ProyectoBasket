f62678a072bafe66e2e1a716e629b4843c1eaf5f
Diego Cuenca Arriols <diego.cuenca@metrica.es>
2019-05-07 09:17:29 +0200

    Cambios leer csv

--- Back/BasketProject/BasketMVC/urls.py
+++ Back/BasketProject/BasketMVC/urls.py
@@ -5,6 +5,7 @@ from BasketMVC import views
 urlpatterns = [
     path('', views.index, name = 'index'),
     path('mostrar_jugadores', views.Mostrar, name = 'Mostrar'),
-    path('upload', views.upload_partido, name='upload_partido')
+    path('upload', views.upload_partido, name='upload_partido'),
+    path('formpartido', views.formulario_partido, name="formulario_partido")
 ]
 
--- Back/BasketProject/BasketMVC/views.py
+++ Back/BasketProject/BasketMVC/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from BasketMVC.models import jugador
-from BasketMVC.models import partido
+
+from BasketMVC.models import jugador, equipo, partido
 import csv
 
 
@@ -11,6 +11,9 @@ def index(request):
 
 def Mostrar(request):
     return render(request, 'tabla.html', {'jugadores': jugador.objects.all()})
+
+def formulario_partido(request):
+    return render(request, 'formulario_partido.html', {'equipos': equipo.objects.all()})
 
 
 def upload_partido(request):
@@ -22,11 +25,31 @@ def upload_partido(request):
 
     ## Obtenemos la información del partido ##
 
+    data_form = request.POST.dict()
+    local = data_form.get("equipo_local")
+    visitante = data_form.get("equipo_visit")
+    id1 = equipo(nombre=local)
+    id2 = equipo(nombre=visitante)
+
     ## Creamos y guardamos el objeto partido ##
+    p = partido(
+        equipo1 = id1,
+        equipo2 = id2,
+        fecha = row[1][5],
+        localizacion = id1.sede,
+        fase = data_form.get("fase"),
+        cuarto1 = row[6][1] + row[7][1],
+        cuarto2 = row[6][2] + row[7][2],
+        cuarto3 = row[6][3] + row[7][3],
+        cuarto4 = row[6][4] + row[7][4],
+        tanteo_final = row[6][5] + row[7][5])
+    p.save()
 
     ## Asociamos el listado de jugadores al partido ##
+    
 
     ## Rellenamos la tabla stats-jugador ##
+
 
     ## Rellenamos la tabla stats-partido ##
 
