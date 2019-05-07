9ee667638e251ef5ab35c09d1236b354d78eaf9f
Frederick23 <35486596+Frederick23@users.noreply.github.com>
2019-05-07 16:19:15 +0200

    Prueba de extracción

--- Back/BasketProject/BasketMVC/read_csv.py
+++ Back/BasketProject/BasketMVC/read_csv.py
@@ -1,11 +1,23 @@
 import csv
 import re
 
+
+# Info básica del partido (localización
 info_partido = []
+# Puntos por periodo de cada equipo
 info_puntos = []
+jugador = []
+# Nombre jugador, estadísticas
+Listadjugadores = []
+# Info de un equipo
 equipo1 = []
+# Info del otro equipo
 equipo2 = []
-rows = []
+# Diccionario "dorsal" - "nombre"
+jugadores = {}
+
+
+# Variables auxiliares
 cont = 0
 cont2 = 0
 rangoA1 = 0
@@ -13,13 +25,16 @@ rangoA2 = 0
 rangoB1 = 0
 rangoB2 = 0
 
-with open('prueba.csv', 'r') as f:
-  reader = csv.reader(f)
-  rows = list(reader)
 
+# Guardamos el fichero en una lista
+with open('prueba.csv', 'r') as f:
+  rows = list(csv.reader(f))
+
+# Buscamos donde empiezan y terminan las listas de jugadores
 for row in rows:
+    # Borra lo no alfanumérico de los string
     fila = re.sub(r'\W+', '', str(row))
-    #print(fila)
+    # Sustituir por equipo1.nombre
     if fila == "Corazonistas":
         rangoB1 = cont2
     elif fila == "CorazonistasResumendeTiros":
@@ -30,10 +45,11 @@ for row in rows:
         rangoA2 = cont2
     cont2 += 1
 
+# Dividimos en sublistas
 for row in rows:
-    if cont in range(0,2):
+    if cont in range(0, 2):
         info_partido.append(row)
-    elif cont in range(4,8):
+    elif cont in range(4, 8):
         info_puntos.append(row)
     elif cont in range(rangoA1, rangoA2):
         equipo1.append(row)
@@ -41,17 +57,23 @@ for row in rows:
         equipo2.append(row)
     cont += 1
 
+# Convertimos los campos vacios en 0
 for fila in equipo1:
     for celda in fila:
-        if(celda == ""):
-            celda = 0
+        if celda == "":
+            equipo1[equipo1.index(fila)][fila.index(celda)] = "0"
+for fila in equipo2:
+    for celda in fila:
+        if celda == "":
+            equipo2[equipo2.index(fila)][fila.index(celda)] = "0"
 
-##print(equipo1)
-
-print("******************")
-
-#for fila in equipo1:
-    #print(fila)
-    #for celda in fila:
-        #print(celda)
-    #print("-----------------")
+# Creamos un diccionario con dorsal-jugador y una lista con nombre jugador + stats
+for fila in range(2, len(equipo1)-2):
+    nombre = re.sub(r'\d+', '', equipo1[fila][0])
+    dorsal = re.sub(r'[^\d]', '', equipo1[fila][0])
+    jugadores[dorsal] = nombre
+    jugador.append(nombre)
+    for celda in range(1, 18):
+        jugador.append(equipo1[fila][celda])
+    Listadjugadores.append(jugador.copy())
+    jugador.clear()
